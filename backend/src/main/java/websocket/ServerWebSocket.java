6f0efd8122036446ae80a881d7d0cc969682219f
LucaBenvenuto <lucabenvenuto@gmail.com>
2020-02-29 16:47:10 -0300

    test websocket

--- backend/src/main/java/websocket/ServerWebSocket.java
+++ backend/src/main/java/websocket/ServerWebSocket.java
@@ -1,38 +1,35 @@
 package websocket;
 
-import org.java_websocket.WebSocket;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.server.WebSocketServer;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
-public class ServerWebSocket extends WebSocketServer {
 
-	@Override
-	public void onOpen(WebSocket conn, ClientHandshake handshake) {
-		// TODO Auto-generated method stub
+@ServerEndpoint("/endpoint")
+public class ServerWebSocket {
+
+	@OnOpen
+	public void onOpen(Session session) {
+		System.out.println("Conexion abierta: " + session.getId());
+	}
+
+	@OnMessage
+	public void onMessage(String txt, Session session) throws IOException {
+		session.getBasicRemote().sendText("hola gil");
+	}
+
+	@OnClose
+	public void onClose(CloseReason reason, Session session) {
 		
 	}
 
-	@Override
-	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onMessage(WebSocket conn, String message) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onError(WebSocket conn, Exception ex) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onStart() {
-		// TODO Auto-generated method stub
+	@OnError
+	public void onError(Session session, Throwable t) {
 		
 	}
 
