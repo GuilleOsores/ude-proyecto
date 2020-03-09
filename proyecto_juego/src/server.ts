2027c09dad03884c02a7c29601a06e34a2dd1b8d
Victoria Aloy <vic.aloy@gmail.com>
2020-03-09 19:15:47 -0300

    conexion automatica

--- proyecto_juego/src/server.ts
+++ proyecto_juego/src/server.ts
@@ -15,46 +15,39 @@ const eventos = Object.values(EVENTOS).reduce(
   {},
 );
 
+let ws:WebSocket = null;
 
 export const addhandler = (event, handler) => { eventos[event].add(handler); };
+
+export async function startWebSocket(){
+  return new Promise((resolve, reject)=>{
+    ws = new WebSocket('ws://localhost:8080/backend/endpoint');
+    ws.onopen = function(){
+    console.log('connected!');
+    resolve();
+  };
+  ws.onmessage = function(msg){
+    try {
+      const data = JSON.parse(msg.data);
+      eventos[data.evento].forEach((h) => h(data));
+    } catch (e) {
+      //console.log(e);
+    }
+  };
+  ws.onclose = function(){
+    console.log('failed!');
+    resolve(startWebSocket());
+  };
+  })
+  
+}
+
+//setInterval(check, 1000);
 
 export const enviar = (evento, data) => {
   try {
     ws.send(JSON.stringify({ evento, ...data }));
   } catch (e) {
-    check();
     //console.log('ws error (si no queres que te joda éste mensaje y no queres '+ 'conectarlo con el server, comentá el send de arriba): ', e);
   }
-
-  var ws = null;
-
-  function start(){
-  
-    ws = new WebSocket('ws://localhost:8080/backend/endpoint');
-    ws.onopen = function(){
-      console.log('connected!');
-    };
-    ws.onmessage = function(msg){
-      try {
-        const data = JSON.parse(msg.data);
-        eventos[data.evento].forEach((h) => h(data));
-      } catch (e) {
-        //console.log(e);
-      }
-    };
-    ws.onclose = function(){
-      console.log('closed!');
-      //reconnect now
-      check();
-    };
-  
-  }
-  
-  function check(){
-    if(!ws || ws.readyState == 3) start();
-  }
-  
-  start();
-  
-  setInterval(check, 1000);
 };
