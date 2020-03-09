5a4187d068e0d96faea26d5cef50bcdad817cde2
Guillermo Osores <guille.osores@gmail.com>
2020-03-09 19:17:50 -0300

    Reconectar servidor (#3)
    * Reconectar servidor
    
    * conexion automatica
    
    Co-authored-by: Victoria Aloy <vic.aloy@gmail.com>

--- proyecto_juego/src/server.ts
+++ proyecto_juego/src/server.ts
@@ -1,5 +1,5 @@
 // let ws: WebSocket = new WebSocket('ws://localhost:8080/backend/endpoint');
-let ws: WebSocket = new WebSocket('ws://localhost:8080/backend/endpoint');
+// let ws: WebSocket = new WebSocket('ws://localhost:8080/backend/endpoint');
 // let ws: WebSocket;// = new WebSocket('ws://localhost:8080/backend/endpoint');
 export const EVENTOS = {
   MOVER_BARCO: 'mb',
@@ -15,24 +15,39 @@ const eventos = Object.values(EVENTOS).reduce(
   {},
 );
 
-export const conectar = () => { ws = new WebSocket('ws://localhost:8080/backend/endpoint'); };
+let ws:WebSocket = null;
 
 export const addhandler = (event, handler) => { eventos[event].add(handler); };
 
-ws.onmessage = (msg) => {
-  try {
-    const data = JSON.parse(msg.data);
-    eventos[data.evento].forEach((h) => h(data));
-  } catch (e) {
-    console.log(e);
-  }
-};
+export async function startWebSocket(){
+  return new Promise((resolve, reject)=>{
+    ws = new WebSocket('ws://localhost:8080/backend/endpoint');
+    ws.onopen = function(){
+    console.log('connected!');
+    resolve();
+  };
+  ws.onmessage = function(msg){
+    try {
+      const data = JSON.parse(msg.data);
+      eventos[data.evento].forEach((h) => h(data));
+    } catch (e) {
+      //console.log(e);
+    }
+  };
+  ws.onclose = function(){
+    console.log('failed!');
+    resolve(startWebSocket());
+  };
+  })
+  
+}
+
+//setInterval(check, 1000);
 
 export const enviar = (evento, data) => {
   try {
     ws.send(JSON.stringify({ evento, ...data }));
   } catch (e) {
-    console.log('ws error (si no queres que te joda éste mensaje y no queres '
-    + 'conectarlo con el server, comentá el send de arriba): ', e);
+    //console.log('ws error (si no queres que te joda éste mensaje y no queres '+ 'conectarlo con el server, comentá el send de arriba): ', e);
   }
 };
