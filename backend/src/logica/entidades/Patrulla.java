ec8a9a5c24f21a32db8d91f32f9c119c3c4978c8
gnovasco <gatutus@gmail.com>
2020-03-13 07:18:04 -0300

    Update Patrulla.java
    se crea constructor que recibe tipoPatrulla, crea una patrulla de tipo tipoPatrulla
    
    //falta implementar.

--- backend/src/logica/entidades/Patrulla.java
+++ backend/src/logica/entidades/Patrulla.java
@@ -1,11 +1,26 @@
 package logica.entidades;
 
+import logica.colecciones.Armas;
+
+import java.util.Properties;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Patrulla extends Vehiculo {
 	
 	private float combustible;
-
-	public Patrulla() {
-		
+	private Armas armas;
+	
+	private String tipoPatrulla;
+	
+	
+	public Patrulla(){
+				
+	}
+	
+	public Patrulla(String tipoPatrulla){
+		//leer de BD o properties
+		//crear una patrulla de tipo tipoPatrulla (grande/chica)		
 	}
 	
 	public Patrulla(float combustible) {
@@ -13,9 +28,11 @@ public class Patrulla extends Vehiculo {
 		this.combustible = combustible;
 	}
 
-	public Patrulla(int id, float x, float y, float velocidad, float velocidadAngular, String spriteVivo, float combustible) {
+	public Patrulla(int id, float x, float y, float velocidad, float velocidadAngular, String spriteVivo, float combustible, Armas armas) {
 		super(id, x, y, velocidad, velocidadAngular, spriteVivo);
 		this.combustible = combustible;
+		this.setArmas(armas);
+		
 	}
 
 	public float getCombustible() {
@@ -24,6 +41,14 @@ public class Patrulla extends Vehiculo {
 
 	public void setCombustible(float combustible) {
 		this.combustible = combustible;
+	}
+
+	public Armas getArmas() {
+		return armas;
+	}
+
+	public void setArmas(Armas armas) {
+		this.armas = armas;
 	}
 	
 }
