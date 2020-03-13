8263b1905caa37e607841e87bc6954fbcb2f8cb6
gnovasco <gatutus@gmail.com>
2020-03-13 14:23:20 -0300

    Update Armas.java
    se agrega getArmasToJson() // falta implementar.

--- backend/src/logica/colecciones/Armas.java
+++ backend/src/logica/colecciones/Armas.java
@@ -3,6 +3,7 @@ package logica.colecciones;
 import java.util.List;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import logica.entidades.Arma;
@@ -17,6 +18,14 @@ public class Armas {
 	
 	public void push(Arma arma) {
 		armas.add(arma);
+	}
+	
+	public JsonArray getArmasToJson() {	
+		JsonArray jsonArray = new JsonArray();
+		
+		//implementar
+		
+		return jsonArray;
 	}
 	
 }
