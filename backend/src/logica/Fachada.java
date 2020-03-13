a14712451bd7a166897288b4c84ae6b1db0274b5
gnovasco <gatutus@gmail.com>
2020-03-13 07:25:15 -0300

    La fachada ahora es Singleton, se crean e implementan metodos.
    Falta terminar de implementar getPartida

--- backend/src/logica/Fachada.java
+++ backend/src/logica/Fachada.java
@@ -1,30 +1,137 @@
 package logica;
 
+import com.google.gson.JsonObject;
+
+import logica.colecciones.Jugadores;
 import logica.colecciones.Partidas;
+import logica.colecciones.Vehiculos;
+import logica.entidades.Jugador;
 import logica.entidades.Partida;
+import logica.entidades.Patrulla;
+import logica.entidades.Pesquero;
+import logica.entidades.Vehiculo;
 
 public class Fachada {
+	
+	private Partida partida;
+	private static Fachada fachada;	
+	private String bandoCreadorDePartida;
+	
+	private Fachada() {
+		partida = new Partida();
+	}
+	
+	
+	public static Fachada getInstanceFachada() {
+		if (fachada == null) {
+			fachada = new Fachada();
+		}
+		return fachada;	
+	}
+	
+	public JsonObject crearPartida(String nickName, String bando, int tamanioEscenarioX, int tamanioEscenarioY, int millasPesca, int tiempo, int cantPeces, int fishFished) {
+		JsonObject json = new JsonObject();
+		String mensaje;
+		
+		if (!partida.getJugadores().isEmpty()) {
+			mensaje = "Ya existe una partida en curso.";			
+		}else {
+			Vehiculo v1 = null, v2 = null;
+			
+			if (bando.equals("PATRULLA")) { 	
+				
+				v1 = new Patrulla("grande");
+				v2 = new Patrulla("chica");	
+				
+			}else if (bando.equals("PESQUERO")) {
+				
+				v1 = new Pesquero("fabrica");
+				v2 = new Pesquero("comun");
+				
+			}
+			
+			Vehiculos vehiculos = new Vehiculos();
+			vehiculos.put(v1);	
+			vehiculos.put(v2);	
+			
+			Jugador jugador = new Jugador(nickName, vehiculos, 0);
+			
+			Jugadores jugadores = partida.getJugadores();
+			jugadores.put(jugador);
+			
+			partida.setJugadores(jugadores);
+			partida.setTamanioEscenarioX(tamanioEscenarioX);
+			partida.setTamanioEscenarioY(tamanioEscenarioY);
+			partida.setMillasPesca(millasPesca);
+			partida.setTiempo(tiempo);
+			partida.setCantPeces(cantPeces);
+			partida.setFishFished(fishFished);
+			
+			bandoCreadorDePartida = bando;
+			
+			mensaje = "OK";
+		}
 
-	private Partidas partidas;
-	
-	public Fachada() {
-		partidas = new Partidas();
+		json.addProperty("mensaje", mensaje);				
+		return json;
 	}
-	
-	public Fachada(Partidas partidas) {
-		this.partidas = partidas;
-	}
-	
-	public Partida crearPartida(String idSesion) {
+
+	public JsonObject unirsePartida(String nickName) {
+		JsonObject json = new JsonObject();
+		String mensaje;
 		
-		return null;
-	}
-	
-	public Partida unirsePartida(String idSesion) {
+		if (partida.getJugadores().isEmpty()) {
+			mensaje = "No hay una partida creada.";			
+		}else {
+			
+			Vehiculo v1 = null, v2 = null;
+			
 		
-		return null;
+			if (bandoCreadorDePartida == "PESQUERO") { 	
+				
+				v1 = new Patrulla("grande");
+				v2 = new Patrulla("chica");	
+				
+			}else {
+				
+				v1 = new Pesquero("fabrica");
+				v2 = new Pesquero("comun");
+				
+			}
+			
+			Vehiculos vehiculos = new Vehiculos();
+			vehiculos.put(v1);	
+			vehiculos.put(v2);	
+			
+			Jugador jugador = new Jugador(nickName, vehiculos, 0);			
+			
+			Jugadores jugadores = partida.getJugadores();
+			jugadores.put(jugador);
+			
+			partida.setJugadores(jugadores);
+				
+			mensaje = "OK";
+		}
+		
+		json.addProperty("mensaje", mensaje);
+		return json;
 	}
 	
-	// public void Pescar(int)
+	public JsonObject getPartida() {
+		JsonObject json = new JsonObject();
+		String mensaje = null;
+		
+		if (partida.getJugadores().isEmpty()) {
+			mensaje = "No hay una partida creada.";			
+		}else {
+			mensaje = "OK";
+			
+			//devolver partida to json, falta armarlo.
+	
+		}
+		
+		json.addProperty("mensaje", mensaje);
+		return json;
+	}
 	
 }
