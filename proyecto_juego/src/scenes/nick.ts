dbcf57138797c55dd60fb03e52b36f7abc762cb9
Guillermo Osores <Apple@MacBook-Pro-de-Pablo.local>
2020-03-15 17:17:22 -0300

    agrego la eleccion de bando

--- proyecto_juego/src/scenes/nick.ts
+++ proyecto_juego/src/scenes/nick.ts
@@ -3,8 +3,15 @@ import axios from 'axios';
 import * as Phaser from 'phaser';
 import InputText from 'phaser3-rex-plugins/plugins/inputtext.js';
 
+
 export class Nick extends Phaser.Scene {
   goNick: any;
+
+  bando: string;
+
+  btnPatrulla: Phaser.GameObjects.Text;
+
+  btnPesquero: Phaser.GameObjects.Text;
 
   btnContinuar: Phaser.GameObjects.Text;
 
@@ -29,41 +36,76 @@ export class Nick extends Phaser.Scene {
       type: 'text', placeholder: 'nick', backgroundColor: 'black', color: '#ffffff', align: 'center',
     });
 
+    this.btnPatrulla = new Phaser.GameObjects.Text(this, 0, 0, 'Patrullas', {});
+    this.btnPesquero = new Phaser.GameObjects.Text(this, 0, 0, 'Pesqueros', {});
+
     this.btnContinuar = new Phaser.GameObjects.Text(this, 0, 0, 'Continuar', {});
 
     this.goNick.setPosition(
       width / 2,
       height / 2,
     );
+
+    this.btnPatrulla.setPosition(
+      (width - this.btnPatrulla.width - 100) / 2,
+      (height - this.btnPatrulla.height) / 2 + 50,
+    );
+
+    this.btnPesquero.setPosition(
+      (width - this.btnPesquero.width + 100) / 2,
+      (height - this.btnPesquero.height) / 2 + 50,
+    );
+
     this.btnContinuar.setPosition(
       (width - this.btnContinuar.width) / 2,
-      (height - this.btnContinuar.height) / 2 + 50,
+      (height - this.btnContinuar.height) / 2 + 100,
     );
 
     this.goNick.setText('player2');
+    this.btnPatrulla.setInteractive();
+    this.btnPesquero.setInteractive();
     this.btnContinuar.setInteractive();
 
     this.add.existing(this.goNick);
+    if (this.crearPartida) {
+      this.add.existing(this.btnPatrulla);
+      this.add.existing(this.btnPesquero);
+    }
     this.add.existing(this.btnContinuar);
 
     this.input.on('gameobjectdown', this.clickHandler);
   }
 
   clickHandler = async (pointer, gameObject: Phaser.GameObjects.GameObject) => {
-    if (gameObject === this.btnContinuar && this.goNick.text !== '') {
+    try {
+      if (gameObject === this.btnContinuar) {
+        if (!this.goNick.text) throw new Error('Debe elegir un nombre');
+        if (!this.bando && this.crearPartida) throw new Error('Debe elegir un bando');
 
-      if (gameObject === this.btnContinuar && this.goNick.text !== '') {
-        let res ;
         if (this.crearPartida) {
           // await axios('http://localhost:8080/backend/crearpartida', { method: 'POST', data: { nickName: this.goNick.text, bando: 'PATRULLA' } });
-           res = await axios(`http://localhost:8080/backend/crearpartida?nickName=${this.goNick.text}&bando=PATRULLA`);
+          await axios(`http://localhost:8080/backend/crearpartida?nickName=${this.goNick.text}&bando=${this.bando}`);
         } else {
           // await axios('http://localhost:8080/backend/unirsepartida', { method: 'POST' });
-           res = await axios(`http://localhost:8080/backend/unirsepartida?nickName=${this.goNick.text}`);
+          await axios(`http://localhost:8080/backend/unirsepartida?nickName=${this.goNick.text}`);
         }
         // console.log(res);
 
         this.scene.start('Espera', { nick: this.goNick.text });
+      } else if (gameObject === this.btnPatrulla) {
+        this.bando = 'PATRULLA';
+        this.btnPatrulla.setBackgroundColor('#486529');
+        this.btnPesquero.setBackgroundColor('');
+      } else if (gameObject === this.btnPesquero) {
+        this.bando = 'PESQUERO';
+        this.btnPatrulla.setBackgroundColor('');
+        this.btnPesquero.setBackgroundColor('#486529');
+      }
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.mensaje);
+      } else {
+        alert(error.message);
       }
     }
   }
