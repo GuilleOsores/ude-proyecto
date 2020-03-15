b56fa0dd239808c0f093950a172e20af4609b7fe
Guillermo Osores <Apple@MacBook-Pro-de-Pablo.local>
2020-03-15 17:47:13 -0300

    agrego getpartida endpoint

--- proyecto_juego/src/config/index.ts
+++ proyecto_juego/src/config/index.ts
@@ -3,6 +3,7 @@ const BACKEND_WS = 'ws://localhost:8080/backend/endpoint';
 
 export const endpoint = {
   crearPartida: (nickName, bando) => `${BACKEND_BASE_URL}/crearpartida?nickName=${nickName}&bando=${bando}`,
+  getPartida: () => `${BACKEND_BASE_URL}/getpartida`,
   unirsePartida: (nickName) => `${BACKEND_BASE_URL}/unirsepartida?nickName=${nickName}`,
   ws: () => BACKEND_WS,
 };
