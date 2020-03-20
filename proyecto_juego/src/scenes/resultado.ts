a85610597fc6139461e7ffa842d829ff20f7a4fb
Guillermo Osores <Apple@MacBook-Pro-de-Pablo.local>
2020-03-20 12:04:42 -0300

    volver a jugar

--- proyecto_juego/src/scenes/resultado.ts
+++ proyecto_juego/src/scenes/resultado.ts
@@ -1,10 +1,13 @@
 import * as Phaser from 'phaser';
+import * as server from '../server';
 
 
 export class Resultado extends Phaser.Scene {
   jugadorLocalNick: String;
 
   ganador: String;
+
+  continuar: Phaser.GameObjects.Text;
 
   constructor() {
     super('Resultado');
@@ -17,6 +20,20 @@ export class Resultado extends Phaser.Scene {
 
   public create() {
     const text = this.add.text(this.game.canvas.width / 2, this.game.canvas.height / 2, this.jugadorLocalNick === this.ganador ? 'Has Ganado!!!!' : 'Sos un perdedor');
+    this.continuar = this.add.text(this.game.canvas.width / 2, this.game.canvas.height / 2 + 50, 'Continuar');
+    this.continuar.setInteractive();
     text.setColor('red');
+    this.continuar.setColor('blue');
+    this.input.on('gameobjectdown', this.clickHandler);
+  }
+
+  clickHandler = async () => {
+    try {
+      server.removerListeners();
+      server.desconectarWs();
+      await server.finalizarPartida();
+    } finally {
+      this.scene.start('Main');
+    }
   }
 }
