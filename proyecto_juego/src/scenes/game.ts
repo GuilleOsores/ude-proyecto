77f49bdd49eeb7d2d41ee74a9186e7beb8474964
Guillermo Osores <guille.osores@gmail.com>
2020-04-25 12:22:16 -0300

    Update game.ts

--- proyecto_juego/src/scenes/game.ts
+++ proyecto_juego/src/scenes/game.ts
@@ -43,7 +43,6 @@ export class Game extends Phaser.Scene {
   }
 
   public init(data: any) {
-    console.log(data.tormentas)
     this.sceneConfig = data;
     this.jugadorLocal.nick = data.nick;
     this.jugadorRemoto.nick = this.sceneConfig.jugadores.find((j) => j.nick !== data.nick).nick;
